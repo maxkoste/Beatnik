4c62917ea366fcaf56e9253a3776d42dd856ef9d
Love Skön (Home) <loveskon335@gmail.com>
2025-03-26 11:46:13 +0100

    Added a mix of implementations and temporary implementations of the GUI elements

--- app/src/main/java/controller/Main.java
+++ app/src/main/java/controller/Main.java
@@ -7,25 +7,11 @@ import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import be.tarsos.dsp.effects.DelayEffect; //test to see that TarsosDSP is working
+import view.MainFrame;
 
-public class Main extends Application{
-
-    @Override
-    //This is just a test to get a window working and see that
-    //JavaFx is running correctly, will be removed. 
-    public void start(Stage primaryStage) throws Exception {
-
-        Group root = new Group();
-        Scene scene = new Scene(root, Color.BEIGE);
-
-        Image flowers = new Image("flowers.JPG");
-        primaryStage.getIcons().add(flowers);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("My first Window!");
-        primaryStage.show();
-    }
+public class Main {
 
     public static void main(String[] args) {
-        launch(args);
+        Application.launch(MainFrame.class, args); // Put on GUI-Thread
     }
 }
