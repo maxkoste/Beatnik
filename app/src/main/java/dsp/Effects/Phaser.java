a2f93e4e2a800739967a12d8ac2f9312eb70dd11
Max Koste <60856309+maxkoste@users.noreply.github.com>
2025-03-27 09:32:12 +0100

    basic architecture for the DSP engine

---
+++ app/src/main/java/dsp/Effects/Phaser.java
@@ -0,0 +1,9 @@
+package dsp.Effects;
+
+//This class represents the pahser implementation
+public class Phaser implements AudioEffect{
+
+    @Override
+    public void process(float[] samples) {
+    }
+}
