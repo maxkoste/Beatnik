0179cbc9e468f2281e3b8ad1870e2a1d05d239c8
Max Koste <60856309+maxkoste@users.noreply.github.com>
2025-03-29 15:45:02 +0100

    Pause/Play now working! Some artifacts can be heard when resuming playing... so this might not be the ultimate solution. Also Volume level is not remembered inbetween pausing and playing

--- app/src/main/java/view/MainFrame.java
+++ app/src/main/java/view/MainFrame.java
@@ -323,7 +323,6 @@ public class MainFrame implements EventHandler<ActionEvent> {
     // listener for setting the volume
     masterVolume.valueProperty().addListener((observable, oldValue, newValue) -> {
       float volume = newValue.floatValue();
-      System.out.println("Master : " + volume);
       controller.setMasterVolume(volume);
     });
 
