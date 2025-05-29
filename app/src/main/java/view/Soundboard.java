dbde2fecc4e060ea0ba8e53dc8e663b39e418a39
Max Koste <60856309+maxkoste@users.noreply.github.com>
2025-05-29 12:21:45 +0200

    Added a separate .css file for a white theme

--- app/src/main/java/view/Soundboard.java
+++ app/src/main/java/view/Soundboard.java
@@ -16,106 +16,110 @@ import javafx.scene.layout.RowConstraints;
 import javafx.stage.Stage;
 
 public class Soundboard implements EventHandler<ActionEvent> {
-    private Controller controller;
-    private ImageView[] images;
-    private Scene soundbarScene;
-    private int gridSize;
-    private Stage stage;
+	private Controller controller;
+	private ImageView[] images;
+	private Scene soundbarScene;
+	private int gridSize;
+	private Stage stage;
 
-    public Soundboard(Controller controller) {
+	public Soundboard(Controller controller) {
 
-        this.gridSize = 4;
-        this.controller = controller;
+		this.gridSize = 4;
+		this.controller = controller;
 
-        initializeSoundBoard();
-    }
+		initializeSoundBoard();
+	}
 
-    private void initializeSoundBoard() {
-        this.stage = new Stage();
-        stage.setTitle("Soundboard");
-        stage.setResizable(false);
-        GridPane grid = new GridPane();
+	private void initializeSoundBoard() {
+		this.stage = new Stage();
+		this.stage.setAlwaysOnTop(true);
+		stage.setTitle("Soundboard");
+		stage.setResizable(false);
+		GridPane grid = new GridPane();
 
-        for (int i = 0; i < this.gridSize; i++) {
-            ColumnConstraints colConstraints = new ColumnConstraints();
-            colConstraints.setPercentWidth(100.0 / gridSize);
-            colConstraints.setFillWidth(true);
-            grid.getColumnConstraints().add(colConstraints);
-        }
+		for (int i = 0; i < this.gridSize; i++) {
+			ColumnConstraints colConstraints = new ColumnConstraints();
+			colConstraints.setPercentWidth(100.0 / gridSize);
+			colConstraints.setFillWidth(true);
+			grid.getColumnConstraints().add(colConstraints);
+		}
 
-        for (int i = 0; i < this.gridSize; i++) {
-            RowConstraints rowConstraints = new RowConstraints();
-            rowConstraints.setPercentHeight(100.0 / gridSize);
-            rowConstraints.setFillHeight(true);
-            grid.getRowConstraints().add(rowConstraints);
-        }
+		for (int i = 0; i < this.gridSize; i++) {
+			RowConstraints rowConstraints = new RowConstraints();
+			rowConstraints.setPercentHeight(100.0 / gridSize);
+			rowConstraints.setFillHeight(true);
+			grid.getRowConstraints().add(rowConstraints);
+		}
 
-        grid.setGridLinesVisible(true);
+		grid.setGridLinesVisible(true);
 
-        this.soundbarScene = new Scene(grid, 600, 600);
-        soundbarScene.getStylesheets().addAll("soundboard.css");
-        stage.setScene(soundbarScene);
+		this.soundbarScene = new Scene(grid, 600, 600);
+		soundbarScene.getStylesheets().addAll("soundboard.css");
+		stage.setScene(soundbarScene);
 
-        Button[] buttons = new Button[4];
-        this.images = new ImageView[8];
+		Button[] buttons = new Button[4];
+		this.images = new ImageView[8];
 
-        Button btn1 = new Button("1");
-        buttons[0] = btn1;
-        Button btn2 = new Button("2");
-        buttons[1] = btn2;
-        Button btn3 = new Button("3");
-        buttons[2] = btn3;
-        Button btn4 = new Button("4");
-        buttons[3] = btn4;
+		Button btn1 = new Button("1");
+		buttons[0] = btn1;
+		Button btn2 = new Button("2");
+		buttons[1] = btn2;
+		Button btn3 = new Button("3");
+		buttons[2] = btn3;
+		Button btn4 = new Button("4");
+		buttons[3] = btn4;
 
-        int counter = 0;
-        String[] resources = { "/Symbols/daddy.png", "/Symbols/fart.png", "/Symbols/horns.png", "/Symbols/yipi.png" };
+		int counter = 0;
+		String[] resources = { "/Symbols/daddy.png", "/Symbols/fart.png", "/Symbols/horns.png", "/Symbols/yipi.png" };
 
-        for (Button button : buttons) {
-            button.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
-            button.setMinSize(Double.MIN_VALUE, Double.MIN_VALUE);
-            button.setScaleX(0.6);
-            button.setScaleY(0.6);
-            button.setText("");
-            ImageView image = new ImageView(new Image(resources[counter]));
-            counter++;
-            image.setScaleX(0.2);
-            image.setScaleY(0.2);
-            image.setFitWidth(button.getWidth());
-            image.setFitHeight(button.getHeight());
-            image.setPreserveRatio(true);
-            button.setGraphic(image);
-        }
+		for (Button button : buttons) {
+			button.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+			button.setMinSize(Double.MIN_VALUE, Double.MIN_VALUE);
+			button.setScaleX(0.6);
+			button.setScaleY(0.6);
+			button.setText("");
+			ImageView image = new ImageView(new Image(resources[counter]));
+			counter++;
+			image.setScaleX(0.2);
+			image.setScaleY(0.2);
+			image.setFitWidth(button.getWidth());
+			image.setFitHeight(button.getHeight());
+			image.setPreserveRatio(true);
+			button.setGraphic(image);
+		}
 
-        btn1.setOnAction(e -> controller.playSoundEffect(1));
-        btn2.setOnAction(e -> controller.playSoundEffect(2));
-        btn3.setOnAction(e -> controller.playSoundEffect(3));
-        btn4.setOnAction(e -> controller.playSoundEffect(4));
+		btn1.setOnAction(e -> controller.playSoundEffect(1));
+		btn2.setOnAction(e -> controller.playSoundEffect(2));
+		btn3.setOnAction(e -> controller.playSoundEffect(3));
+		btn4.setOnAction(e -> controller.playSoundEffect(4));
 
-        grid.add(btn1, 1, 1);
-        grid.add(btn2, 1, 2);
-        grid.add(btn3, 2, 1);
-        grid.add(btn4, 2, 2);
+		grid.add(btn1, 1, 1);
+		grid.add(btn2, 1, 2);
+		grid.add(btn3, 2, 1);
+		grid.add(btn4, 2, 2);
 
-        // Slider
-        Slider masterVolume = new Slider();
-        masterVolume.setOrientation(Orientation.VERTICAL);
-        masterVolume.setMax(100.0);
-        masterVolume.setBlockIncrement(20);
-        masterVolume.setShowTickMarks(true);
-        masterVolume.setShowTickLabels(true);
-        masterVolume.setMinorTickCount(0);
-        masterVolume.valueProperty().addListener((observable, oldValue, newValue) -> {
-            controller.setSoundboardVolume(newValue.floatValue() / 100);
-        });
-        masterVolume.setValue(50);
+		// Slider
+		Slider masterVolume = new Slider();
+		masterVolume.setOrientation(Orientation.VERTICAL);
+		masterVolume.setMax(100.0);
+		masterVolume.setBlockIncrement(20);
+		masterVolume.setShowTickMarks(true);
+		masterVolume.setShowTickLabels(true);
+		masterVolume.setMinorTickCount(0);
+		masterVolume.valueProperty().addListener((observable, oldValue, newValue) -> {
+			controller.setSoundboardVolume(newValue.floatValue() / 100);
+		});
+		masterVolume.setValue(50);
 
-        GridPane.setRowSpan(masterVolume, 2);
-        grid.add(masterVolume, 3, 1);
-    }
+		GridPane.setRowSpan(masterVolume, 2);
+		grid.add(masterVolume, 3, 1);
+	}
 
-    @Override
-    public void handle(ActionEvent arg0) {
-        this.stage.show();
-    }
+	@Override
+	public void handle(ActionEvent arg0) {
+		this.stage.show();
+	}
+
+	public void exit() {
+	}
 }
